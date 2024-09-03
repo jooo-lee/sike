6710ad194aec61786efdbe6d1ee4f6e637a44e01
Joseph Lee <josephlee3004@gmail.com>
2024-09-03 13:46:42 -0400

    Create global styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from 'react-router-dom';
 
+import GlobalStyle from './globalStyles.js';
 import Navbar from './components/Navbar/Navbar.jsx';
 
 function App() {
   return (
     <>
+      <GlobalStyle />
       <Navbar />
       <Outlet />
     </>
