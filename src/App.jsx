5091c362c1d2d9fdce984544c051de1b7c84a659
Joseph Lee <josephlee3004@gmail.com>
2024-10-31 12:04:05 -0400

    Store cart using localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 import { Outlet } from 'react-router-dom';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import GlobalStyle from './globalStyles.js';
 import Navbar from './components/Navbar/Navbar.jsx';
 
 const App = () => {
-  const [cart, setCart] = useState([]);
+  const [cart, setCart] = useState(() => {
+    // Retrieve and use stored cart if applicable
+    const storedCart = JSON.parse(localStorage.getItem('cart'));
+    return storedCart ? storedCart : [];
+  });
+
+  // Keep localStorage cart in sync
+  useEffect(() => {
+    localStorage.setItem('cart', JSON.stringify(cart));
+  }, [cart]);
 
   const addToCart = (productId, quantity) => {
     // If product is already in the cart, add new quantity to previous quantity
